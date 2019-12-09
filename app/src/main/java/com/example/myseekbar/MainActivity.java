4cd059b8417df8e418c753c618ef59c9d042129b
Cimara Sá <cimara.oliveira@amcom.com.br>
2019-12-09 17:15:45 -0300

    commit initial

---
+++ app/src/main/java/com/example/myseekbar/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.myseekbar;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekBar;
+    TextView porcentagemTv;
+    NumberFormat formatadorPercentual;
+    TextView taxaTv;
+    TextView totalTv;
+    EditText valorEd;
+    double valor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        alimentarVariaveis();
+        sincronizarTextView();
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                sincronizarTextView();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                sincronizarValor();
+            }
+        });
+
+    }
+
+    public void alimentarVariaveis(){
+        seekBar = findViewById(R.id.seekbar);
+        porcentagemTv = findViewById(R.id.percentTextView);
+        formatadorPercentual = NumberFormat.getPercentInstance();
+
+        valorEd = findViewById(R.id.valorEd);
+        taxaTv = findViewById(R.id.taxaTotalTv);
+        //totalTv = findViewById(R.id.totalTv);
+    }
+    public void sincronizarTextView() {
+        valor = seekBar.getProgress();
+        this.porcentagemTv.setText(formatadorPercentual.format(valor / 100.0));
+    }
+
+    private void sincronizarValor() {
+        double porc = valor;
+        porc = porc / 100.0;
+        double valor = valorEd.getText().toString().equals("") ? 0 : Double.parseDouble(valorEd.getText().toString());
+        double taxa = porc * valor;
+        taxaTv.setText(Double.toString(taxa));
+        totalTv.setText(Double.toString(taxa + valor));
+    }
+}
